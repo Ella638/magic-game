bcbe0f4ebf9de5349cc0ca1755f44d4003c255b1
LaceyZheng <laceyzheng2017@gmail.com>
2022-11-03 15:12:08 -0700

    first commit

---
+++ magic-memory/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import './App.css';
+import SingleCard from'./components/SingleCard';
+const cardImages=[
+  {"src": "/img/helmet-1.png" },
+  {"src": "/img/potion-1.png" },
+  {"src": "/img/ring-1.png" },
+  {"src": "/img/scroll-1.png" },
+  {"src": "/img/shield-1.png" },
+  {"src": "/img/sword-1.png" },
+]
+
+
+function App() {
+  const[cards,setCards]=useState([])
+  const[turns,setTurns]=useState(0)
+
+
+
+
+  const shuffleCards=()=>{
+    const shuffleCards=[...cardImages,...cardImages]
+    .sort(()=>Math.random()-0.5)
+    .map((card)=>({...card, id: Math.random()}))
+    setCards(shuffleCards)
+    setTurns(0)
+  }
+
+  console.log(cards,turns)
+
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map(card=> (
+         <SingleCard  key={card.id} card={card} />
+
+        
+        
+    ))}
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ magic-memory/src/components/SingleCard.js
@@ -0,0 +1,23 @@
+import'./SingleCard.css'
+
+export default function SingleCard({card}){
+    return(
+
+        <div className ="card" >
+            <div>
+
+               <img className ="front" src={card.src} alt="card front" />
+
+                <img className="back" src="/img/cover.png" alt="card front" />
+
+
+
+            </div>
+
+
+        </div>
+
+
+
+    )
+}
